d6ee07cd5708e6baa8363f212bf82719b88ed6b9
Dmitry Barskov <barskoidim@gmail.com>
2019-01-18 11:15:18 +0300

    move stories from main page to stories, add user_path, add erb code

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :stories
   root to: 'pages#home'
   devise_for :users
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
