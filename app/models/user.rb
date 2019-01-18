672bcc1813cc489e80b1d823da0bb8eb920b9937
Dmitry Barskov <barskoidim@gmail.com>
2019-01-18 17:22:11 +0300

    add dependencies for User: stories, comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :stories , dependent: :delete_all
+  has_many :comments, dependent: :delete_all
 end
